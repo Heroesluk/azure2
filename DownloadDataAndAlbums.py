d4effc12b9c9953d27960353f0ea34724f154e00
heroesluk <heroesluk@gmail.com>
2022-11-16 21:49:18 +0100

    works for now

--- DownloadDataAndAlbums.py
+++ DownloadDataAndAlbums.py
@@ -3,49 +3,53 @@ import json
 import os
 from sys import getsizeof
 from PIL import Image
-
+import asyncio
+import aiohttp
 import time
 
 start_time = time.time()
-#heroesluk
-
-def get_albums(user):
-    for i in range(10):
-        with open('Albums/albums{}.json'.format(i), 'w') as f:
-            data = re.get(
-                'http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&limit=200&page={}&user={}&api_key=d6e02ae58fcf6daaea788ce99c879f9c&format=json'.format(
-                    i,user))
 
 
-            albums = data.json()
-            json.dump(albums, f)
+# heroesluk
 
-    #print(data.content)
+# number of albums is multiplied by 1000,
+# since limit above 500 remove image links from response(probably bc of request size?)
+# i multiply n_o_a by 2 to achieve desired number
+def create_albums_json(user, number_of_albums):
+    data_to_append = []
+    for i in range(1, (number_of_albums * 2) + 1):
+        data = re.get(
+            'http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&limit=500&page={}&user={}&api_key=d6e02ae58fcf6daaea788ce99c879f9c&format=json'.format(
+                i, user))
+
+        albums = data.json()
+        data_to_append.append(albums)
+
+    head = data_to_append[0]['topalbums']['album']
+    for i in data_to_append[1:]:
+        head += i['topalbums']['album']
+
+    with open('albums.json', 'w') as f:
+        json.dump(head, f)
 
 
-
-def parse_album_json(name, id_offset):
+def get_all_small_album_img_links():
     links = []
     try:
-        with open('Albums/{}'.format(name), 'r') as file:
+        with open('albums.json', 'r') as file:
             data = json.load(file)
     except FileNotFoundError:
-        print('incorrect directory name: {}'.format(name))
+        print('incorrect directory name: {}')
         exit()
 
-    for ind, album in enumerate(data['topalbums']['album']):
-        temp = album['image'][1]['#text']
+    for ind, album in enumerate(data):
+        temp = album["image"][0]['#text']  # index 0 refers to image size in json,
+        # 1 is to medium, and 2 big - > #text for some reason is where the image link is
 
-
-        if len(temp)>0:
+        if len(temp) > 0:  # check if link actually exist, if no then ignore album
             links.append(temp)
 
-
     return links
-
-
-import asyncio
-import aiohttp
 
 
 async def fetch(session, url):
@@ -53,8 +57,9 @@ async def fetch(session, url):
         return await resp.content.read()
         # Catch HTTP errors/exceptions here
 
-async def fetch_concurrent(urls,offset):
-    count = offset
+#  asynchronously download all images provided list of image links, and name them as index numbers
+async def fetch_concurrent(urls):
+    count = 0
     loop = asyncio.get_event_loop()
     async with aiohttp.ClientSession() as session:
         tasks = []
@@ -64,26 +69,24 @@ async def fetch_concurrent(urls,offset):
         for result in asyncio.as_completed(tasks):
             page = await result
 
-            with open('AlbumCovers/{}.jpg'.format(count),'wb') as f:
+            with open('AlbumCovers/{}.jpg'.format(count), 'wb') as f:
                 f.write(page)
 
-
             count+=1
-            #Do whatever you want with results
 
 
-#asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 def _all():
-    offset = 0
-    for file in os.listdir('Albums/'):
-        urls = parse_album_json(file, offset)
-        asyncio.run(fetch_concurrent(urls,offset))
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    urls = get_all_small_album_img_links()
+    asyncio.run(fetch_concurrent(urls))
 
 
-        print(file, ' completed')
-        offset += 200
+with open('albums.json', 'r') as file:
+    data = json.load(file)
 
+    print(data[2])
 
-
-
+print("--- %s seconds ---" % (time.time() - start_time))
+#TODO: save images based on something other then index, since async make it impossible to guess based on name what album it is
+#i should probably serialize this json
